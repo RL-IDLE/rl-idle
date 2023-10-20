d710af9a361bbadfb0237ab83d08099fe22a5b7c
rharkor <louis@huort.com>
2023-10-20 15:37:10 +0200

    feat: icons

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -16,8 +16,9 @@ function Item({
   return (
     <li
       className={cn(
-        'bg-blue-400 py-4 flex-1 text-center cursor-pointer',
+        'bg-blue-400 py-4 flex-1 text-center cursor-pointer h-24 transition-all duration-200 flex flex-col justify-center items-center',
         'hover:bg-blue-500',
+        '[&>*]:w-10 [&>*]:h-10',
         {
           'bg-blue-500': active,
           'opacity-50 pointer-events-none cursor-default': disabled,
@@ -34,7 +35,11 @@ export default function Navbar() {
   const navigationStore = useNavigationStore();
   return (
     <nav className="">
-      <ul className={cn('flex flex-row justify-between')}>
+      <ul
+        className={cn(
+          'flex flex-row justify-between border-t-2 border-blue-400',
+        )}
+      >
         {pages.map((page) => (
           <Item
             active={navigationStore.page === page.name}
