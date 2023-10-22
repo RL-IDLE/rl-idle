7e9ea89f19e91f0d3528e37033fdc16e51c83504
rharkor <louis@huort.com>
2023-10-22 22:32:19 +0200

    feat(frontend): passive income popup

---
+++ frontend/src/lib/constant/index.ts
@@ -0,0 +1,2 @@
+export const refreshInterval = 200;
+export const popupMinOfflineTime = 0; //60000;
--- frontend/src/lib/utils/index.ts
+++ frontend/src/lib/utils/index.ts
@@ -4,3 +4,71 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Get the time between to dates
+ * @param firstDate First date
+ * @param secondDate Second date
+ * @returns The time between the two dates
+ * @example
+ * getTimeBetween(new Date("2020-01-01"), new Date("2020-01-02")) // 1 day
+ */
+export const getTimeBetween = (
+  firstDate: Date,
+  secondDate: Date,
+  options?: {
+    markAsNowSince?: number;
+  },
+) => {
+  // Time difference in milliseconds
+  const timeDiff = Math.abs(firstDate.getTime() - secondDate.getTime()); // in milliseconds
+
+  // Check for the markAsNowSince
+  const markAsNowSince = options?.markAsNowSince ?? 1000 * 60;
+  if (timeDiff < markAsNowSince) {
+    return 'maintenant';
+  }
+
+  // Define time intervals in milliseconds
+  const intervals = {
+    year: 365.25 * 24 * 60 * 60 * 1000,
+    month: 30.44 * 24 * 60 * 60 * 1000,
+    day: 24 * 60 * 60 * 1000,
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+    second: 1000,
+  };
+  // Calculate the number of intervals elapsed
+  const elapsed = {
+    year: Math.floor(timeDiff / intervals.year),
+    month: Math.floor(timeDiff / intervals.month),
+    day: Math.floor(timeDiff / intervals.day),
+    hour: Math.floor(timeDiff / intervals.hour),
+    minute: Math.floor(timeDiff / intervals.minute),
+    second: Math.floor(timeDiff / intervals.second),
+  };
+  // Determine the appropriate time interval to display
+  let timeUnit;
+  let timeValue;
+  if (elapsed.year > 0) {
+    timeUnit = elapsed.year > 1 ? 'ans' : 'an';
+    timeValue = elapsed.year;
+  } else if (elapsed.month > 0) {
+    timeUnit = elapsed.month > 1 ? 'mois' : 'mois';
+    timeValue = elapsed.month;
+  } else if (elapsed.day > 0) {
+    timeUnit = elapsed.day > 1 ? 'jours' : 'jour';
+    timeValue = elapsed.day;
+  } else if (elapsed.hour > 0) {
+    timeUnit = elapsed.hour > 1 ? 'heures' : 'heure';
+    timeValue = elapsed.hour;
+  } else if (elapsed.minute > 0) {
+    timeUnit = elapsed.minute > 1 ? 'minutes' : 'minute';
+    timeValue = elapsed.minute;
+  } else {
+    timeUnit = elapsed.second > 1 ? 'secondes' : 'seconde';
+    timeValue = elapsed.second;
+  }
+  // Construct and return the time elapsed string
+  return `${timeValue} ${timeUnit}`;
+};
