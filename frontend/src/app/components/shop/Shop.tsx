b3b993f57f1e1c427a4f62a50530dd206d05f406
Thrive <guillaumemengelatte@hotmail.fr>
2023-11-02 11:46:15 +0100

    feat: front shop, front navbar

--- frontend/src/app/components/shop/Shop.tsx
+++ frontend/src/app/components/shop/Shop.tsx
@@ -6,10 +6,11 @@ import { cn } from '@/lib/utils';
 import Decimal from 'break_infinity.js';
 import { logger } from '@/lib/logger';
 import { decimalToHumanReadable } from '@/lib/bignumber';
-import styles from "./shop.module.scss"
+import styles from './shop.module.scss';
 import clickSound from '@/assets/audio/buy-item.wav';
 import { useBalance } from '@/contexts/balance/BalanceUtils';
 import { useState } from 'react';
+import CreditLogo from '@/assets/credits_icon.webp';
 
 export default function Shop() {
   const buyItem = useGameStore((state) => state.actions.buyItem);
@@ -48,6 +49,8 @@ export default function Shop() {
             item.price,
             itemsLevels[item.id] || Decimal.fromString('0'),
           ),
+    level: itemsLevels[item.id] || Decimal.fromString('0'),
+    // moneyPerSecond: (itemsLevels[item.id] * item.moneyPerSecond) || Decimal.fromString('0'),
   }));
 
   const handleBuy = (id: string) => {
@@ -57,14 +60,18 @@ export default function Shop() {
   };
 
   return (
-    <section className={styles.shop + " flex flex-col mt-32 rounded-2xl"}>
-      <ul className="flex flex-col gap-2 overflow-auto touch-pan-y pt-7 items-center">
+    <section className={styles.shop + ' flex flex-col mt-32 rounded-xl pt-5'}>
+      <ul className="flex flex-col gap-2 overflow-auto touch-pan-y items-center rounded-xl pt-3 pb-3">
         {itemsWithPrice.map((item) => (
           <li
             key={item.id}
-            className={cn('flex flex-col gap-2 border p-2 cursor-pointer', {
-              'border-red-500': item.price.gt(balance),
-            })}
+            className={cn(
+              'flex flex-row gap-2 border p-2 cursor-pointer relative',
+              {
+                'opacity-70': item.price.gt(balance),
+                'pointer-events-none': item.price.gt(balance),
+              },
+            )}
             onClick={() => {
               if (item.price.gt(balance)) {
                 logger.debug('Not enough money to buy item');
@@ -74,14 +81,26 @@ export default function Shop() {
             }}
           >
             <img src={item.image} alt={item.name} className="w-12 h-12" />
-            <p className="text-white">
-              {item.name} - {decimalToHumanReadable(item.price)}
-            </p>
-            <p className="text-white">
-              {item.moneyPerSecond.eq(0)
-                ? `x${item.moneyPerClickMult.toString()} per click`
-                : `+${decimalToHumanReadable(item.moneyPerSecond)} per second`}
-            </p>
+            <div className="flex flex-col gap-2">
+              <p className="text-white">{item.name}</p>
+
+              <p className="price text-white flex flex-row gap-1">
+                <img width="20" height="20" src={CreditLogo} alt="credit" />
+                {decimalToHumanReadable(item.price)}
+              </p>
+
+              <p className="text-white">
+                {item.moneyPerSecond.eq(0)
+                  ? `x${item.moneyPerClickMult.toString()} per click`
+                  : `+${decimalToHumanReadable(
+                      item.moneyPerSecond,
+                    )} per second`}
+              </p>
+              <p className="level text-white absolute top-1 right-1">
+                lvl {decimalToHumanReadable(item.level)}
+              </p>
+              <p>{decimalToHumanReadable(item.moneyPerSecond)}</p>
+            </div>
           </li>
         ))}
       </ul>
