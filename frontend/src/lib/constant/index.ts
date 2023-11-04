ca3650ac4c0ecd1584f206c5cfed29eaf51d1e4e
rharkor <louis@huort.com>
2023-11-04 12:42:55 +0100

    refactor: click interval

--- frontend/src/lib/constant/index.ts
+++ frontend/src/lib/constant/index.ts
@@ -1,3 +1,4 @@
 export const refreshInterval = 200;
 export const popupMinOfflineTime = 60000 * 3; //? 3 minutes
 export const livelinessProbeInterval = 10 * 1000; //? 10 seconds
+export const clickRefreshInterval = 30;
