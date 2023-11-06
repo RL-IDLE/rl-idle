7164aca9b1943470981b35c266175bd9567fd6aa
rharkor <louis@huort.com>
2023-11-06 23:37:21 +0100

    fix(frontend): notifications

--- frontend/src/app/sw-custom.ts
+++ frontend/src/app/sw-custom.ts
@@ -12,7 +12,6 @@ self.importScripts(
 
 // Function to check passive income status and notify the user
 async function checkPassiveIncomeStatus() {
-  console.log('checking passive income status');
   const lastBalance = await localforage.getItem('lastBalance');
   const lastBalanceTime = await localforage.getItem('lastBalanceTime');
   const maxPassiveIncomeInterval = await localforage.getItem(
@@ -22,14 +21,9 @@ async function checkPassiveIncomeStatus() {
   if (lastBalance && lastBalanceTime && maxPassiveIncomeInterval) {
     const currentTime = new Date().getTime();
     const lastTime = parseInt(lastBalanceTime, 10);
-    const interval = 1000 * 10; //parseInt(maxPassiveIncomeInterval, 10);
+    const interval = parseInt(maxPassiveIncomeInterval, 10);
 
     const notificationSent = await localforage.getItem('notificationSent');
-    console.log(
-      '🚀 ~ notif',
-      currentTime - lastTime >= interval,
-      notificationSent,
-    );
     if (currentTime - lastTime >= interval && !notificationSent) {
       // Passive income has stopped, notify the user
       sw.registration.showNotification('Claim your rewards!', {
@@ -49,7 +43,6 @@ setInterval(checkPassiveIncomeStatus, 10000);
 
 sw.addEventListener('message', (event) => {
   if (event.data.kind === 'save') {
-    console.log('saving values');
     localforage.setItem('lastBalance', event.data.lastBalance);
     localforage.setItem('lastBalanceTime', event.data.lastBalanceTime);
     localforage.setItem(
