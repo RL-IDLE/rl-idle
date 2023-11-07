2b94511c40e3ad93aa3f8ecb6035bf3c0571243b
rharkor <louis@huort.com>
2023-11-07 16:20:16 +0100

    refactor: passive popup x balance

--- frontend/src/app/components/Balance.tsx
+++ frontend/src/app/components/Balance.tsx
@@ -4,10 +4,11 @@ import styles from './balance.module.scss';
 import { decimalToHumanReadable } from '@/lib/bignumber';
 import CreditLogo from '@/assets/credits_icon.webp';
 import { useBalance } from '@/contexts/balance/BalanceUtils';
+import { cn } from '@/lib/utils';
 
 export default function Balance() {
   const user = useUserStore((state) => state.user);
-  const { balance } = useBalance();
+  const { balance, difference } = useBalance();
 
   const moneyPerSecond = getMoneyFromInvestmentsPerSeconds(user);
 
@@ -18,8 +19,17 @@ export default function Balance() {
         ' absolute top-0 left-0 z-50 mt-5 ml-5 p-3 flex items-center justify-center rounded-xl flex-wrap'
       }
     >
-      <h2 className="text-center relative w-fit text-white flex flex-col">
-        {decimalToHumanReadable(balance, balance.lt('1000') ? true : false)}
+      <h2 className={cn('text-center relative w-fit text-white flex flex-col')}>
+        <span
+          className={cn('!text-lg', {
+            'animate-bigNumberUpdate': difference === null,
+          })}
+        >
+          {decimalToHumanReadable(
+            difference ? balance.minus(difference) : balance,
+            balance.lt('1000') ? true : false,
+          )}
+        </span>
         <span>+{decimalToHumanReadable(moneyPerSecond)} /s</span>
       </h2>
       <img width="45" height="45" src={CreditLogo} alt="credit" />
