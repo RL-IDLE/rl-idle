1a7e1ea5d429489b33d65d71fb86b84f9fa21299
rharkor <louis@huort.com>
2023-11-09 17:49:15 +0100

    fix: lint

--- backend/src/database/migrations/1699544066026-latestBalance.ts
+++ backend/src/database/migrations/1699544066026-latestBalance.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class LatestBalance1699544066026 implements MigrationInterface {
-    name = 'LatestBalance1699544066026'
+  name = 'LatestBalance1699544066026';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "latestBalance" character varying NOT NULL DEFAULT '0'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "latestBalance" character varying NOT NULL DEFAULT '0'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "latestBalance"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "latestBalance"`);
+  }
 }
