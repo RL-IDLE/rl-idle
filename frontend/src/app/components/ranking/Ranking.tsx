ef84c8f62be797b6b1887cc368c36ddd3673835c
Thrive <guillaumemengelatte@hotmail.fr>
2023-11-09 13:41:53 +0100

    feat: scoreboard

--- frontend/src/app/components/ranking/Ranking.tsx
+++ frontend/src/app/components/ranking/Ranking.tsx
@@ -1,3 +1,45 @@
+import { useUserStore } from "@/contexts/user.store";
+import { useEffect } from "react";
+
 export default function Ranking() {
-  return <section>ranking</section>;
+  const user = useUserStore((state) => state.user);
+  const fakeUsersScores = [
+    {
+      username: 'toto',
+      score: 100,
+      prestige: 
+    },
+    {
+      username: 'titi',
+      score: 200,
+    },
+    {
+      username: 'tata',
+      score: 300,
+    },
+    {
+      username: 'tutu',
+      score: 400,
+    },
+  ];
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+    
+  
+  return (
+    <section>
+      <h1>Classement</h1>
+      <div className="flex flex-col">
+        {fakeUsersScores.map((user, index) => (
+          <div key={index} className="flex justify-between">
+            <p>{index + 1}</p>
+            <p>{user.username}</p>
+            <p>{user.score}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
